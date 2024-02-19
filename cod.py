c0f0b5d516d5a234b7c9fdf49b668427534a1ecf
Duvan Ramirez <duvanx82018@gmail.com>
2024-02-19 14:46:42 -0500

    ultimacorrecionparacir

--- cod.py
+++ cod.py
@@ -3,46 +3,59 @@ import math
 
 print("\t\tPráctica 1: Ubicar un punto en el plano cartesiano\n")
 
-x = float(input("Por favor ingrese el valor de X\n"))
+print("Como instruciones debe ingresar primero el radio de la circunferencia, luego de esto ingrese las coordenadas del centro de circunferencia para que asi en el ultimo paso ingrese el punto que desea ubicar en la circunferencia.\n")
 
-y = float(input("Por favor ingrese el valor de Y\n"))
+#Variables
 
-if x > 0 and y > 0:
+radio = float(input("Por favor ingrese el radio de la circunferencia\n"))
+
+x1 = float(input("Por favor ingrese el valor del punto centro en el eje X\n"))
+
+y1 = float(input("Por favor ingrese el valor del punto centro en el eje Y\n"))
+
+x2 = float(input("Por favor ingrese el valor para el punto de X\n"))
+
+y2 = float(input("Por favor ingrese el valor para el punto de Y\n"))
+
+
+#Funcionamiento para indicar area del punto dado
+
+if x2 > 0 and y2 > 0:
       print("El punto se encuentra en el primer cuadrante del plano cartesiano")
-if x < 0 and y > 0:
+if x2 < 0 and y2 > 0:
       print("El punto se encuentra en el segundo cuadrante del plano cartesiano")
-if x < 0 and y < 0:
+if x2 < 0 and y2 < 0:
       print("El punto se encuentra en el tercer cuadrante del plano cartesiano")
-if x > 0 and y < 0:
+if x2 > 0 and y2 < 0:
       print("El punto se encuentra en el cuarto cuadrante del plano cartesiano")
-if x == 0 and y == 0:
+if x2 == 0 and y2 == 0:
       print("El punto se encuentra en el origen del plano cartesiano")
-if x == 0:
+if x2 == 0:
       print("El punto se encuentra en el eje Y del plano cartesiano")
-if y == 0:
+if y2 == 0:
       print("El punto se encuentra en el eje X del plano cartesiano")
 
-if x == y:
+if x2 == y2:
       print("El punto se encuentra en la línea X = Y")
-if x > y:
+if x2 > y2:
       print("El punto se encuentra por debajo de la línea X = Y")
 else:
       print("El punto se encuentra por encima de la línea X = Y")
 
-if x == -y:
-      print("El punto se encuentra en la línea X = -Y")
-if x > -y:
-      print("El punto se encuentra por debajo de la línea X = -Y")
+
+distancia = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+
+#Calcular distancia del punto hasta el centro
+if distancia < radio:
+    print("El punto está dentro de la circunferencia")
+elif distancia == radio:
+    print("El punto está en la circunferencia")
 else:
-      print("El punto se encuentra por encima de la línea X = -Y")
+    print("El punto está fuera de la circunferencia")
 
-v_x = int(x**2)
-v_y = int(y**2)
 
-resultado = v_x + v_y
 
-r_sqrt = round(sqrt(resultado), 1)
+circunf = round((radio * 2 * math.pi), 1)
 
-cir = round((r_sqrt * math.pi), 1)
-
-print("El resultado de la circunferencia es ", cir, " y su distancia es ", r_sqrt)
+print("El resultado de la circunferencia es ", circunf, " y su distancia es ", radio)
